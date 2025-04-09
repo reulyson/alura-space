fb6d5ded34aff959bfc12eda4dec7608440b01f1
Reulyson Wanzeler <reulyson@gmail.com>
2025-04-09 17:46:26 -0400

    Cria campos de login e cadastro no tela do site. Organiza os códigos repetidos

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import login, cadastro
 
 ''' Caminhos para as páginas da galeria '''
 urlpatterns = [
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request): # Função para renderizar a página de login
+    return render(request, 'usuarios/login.html')
+
+def cadastro(request): # Função para renderizar a página de cadastro
+    return render(request, 'usuarios/cadastro.html')
