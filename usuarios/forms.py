beacb119d528fe28ba919a343a51ccc39b891ef0
Reulyson Wanzeler <reulyson@gmail.com>
2025-04-10 16:04:05 -0400

    Sessão de cadastro criada

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -7,12 +7,74 @@ class LoginForm(forms.Form):
     nome_login = forms.CharField(
         label='Nome de Login', # Rotulo
         required=True, # Obrigatorio
-        max_length=100 # Tamanho
+        max_length=100, # Tamanho
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: João Silva'
+            }
+        ) 
     )
 
     senha = forms.CharField(
         label='Senha', 
         required=True,
         max_length=70,
-        widget=forms.PasswordInput # Campo de senha
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        ) # Campo de senha
+    )
+
+class CadastroForm(forms.Form):
+
+    # Campos
+    nome_cadastro = forms.CharField(
+        label='Nome completo', # Rotulo
+        required=True, # Obrigatorio
+        max_length=100, # Tamanho
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: João Silva'
+            }
+        ) 
+    )
+
+    email = forms.EmailField(
+        label='Email', 
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: joaosilva@xpto.com'
+            }
+        ) # Campo de senha
+    )
+
+    senha_1 = forms.CharField(
+        label='Senha', 
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        ) # Campo de senha
+    )
+
+    senha_2 = forms.CharField(
+        label='Confirmação de senha', 
+        required=True,  
+        max_length=70,  
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente'
+            }
+        )
     )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .forms import LoginForm
+from django.shortcuts import render, redirect
+from .forms import LoginForm, CadastroForm
 
 def login(request): # Função para renderizar a página de login
 
@@ -7,4 +7,13 @@ def login(request): # Função para renderizar a página de login
     return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request): # Função para renderizar a página de cadastro
-    return render(request, 'usuarios/cadastro.html')
+
+    form = CadastroForm()
+
+    if request.method == 'POST': # Verifica se o método da requisição é POST
+        form = CadastroForm(request.POST) # Cria um formulário com os dados da requisição
+        if form['senha_1'].value() != form['senha_2'].value(): # Verifica se as senhas são iguais
+            return redirect('cadastro')
+        
+
+    return render(request, 'usuarios/cadastro.html', {'form': form})
